614c059291cd00b1caa0537e42dd0c2175ba7606
smirnov-am <msc.smirnov.am@gmail.com>
2018-03-12 23:04:25 +0100

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='pytopicrank',
+      version='0.1',
+      description='Implementation of TopicRank algorithm for keyphrase extraction from text',
+      url='https://github.com/smirnov-am/pytopicrank',
+      author='Alexey Smirnov',
+      author_email='msc.smirnov.am@gmail.com',
+      license='MIT',
+      install_requires=['decorator==4.2.1',
+                        'langdetect==1.0.7', 'networkx==2.1', 'nltk==3.2.5',
+                        'numpy==1.14.1', 'scikit-learn==0.19.1', 'scipy==1.0.0', 'six==1.11.0'],
+      packages=find_packages())
